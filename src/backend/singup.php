8ce562cc517395c4c1231832395e87d9e43a63da
Diego Castaño <diegoalj46@gmail.com>
2024-05-03 16:39:14 -0500

    Corte 2

---
+++ src/backend/singup.php
@@ -0,0 +1,41 @@
+<?php
+    include('../../config/databases.php');
+
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
+
+    $sql_validate_email = " SELECT * FROM users WHERE email= '$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total = pg_num_rows($result);
+
+    if($total >0){
+        echo "<script>alert('Email already exists')</script>" ;
+        header("refresh:0;url=../signin.html");
+
+    }else{
+
+        $sql = "
+        INSERT INTO users2 (fullname, email, password) 
+            VALUES ('$fullname', '$email','$enc_pass')
+        ";
+
+        $ans = pg_query($conn , $sql);
+        if ($ans){
+            echo "<script>alert('User has been registered')</script>";
+            header("refresh:0;url=../signin.html");
+        }else{
+            
+            echo "Error: " . pg_last_error();
+        }
+    }
+
+   
+
+    //Close connection
+    pg_close($conn)
+
+    
+
+?>
